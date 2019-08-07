1a5406594136effdcb24e970b4cdd97b132d3ea2
TheFunnyPapa <gabrielragy@gmail.com>
2019-08-07 15:16:09 +0300

    v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CustomerSupportComponent } from './customer-support/customer-support.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"about", component: AboutComponent},
+  {path:"customer-support", component: CustomerSupportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const yo = 'Jagshemash';
